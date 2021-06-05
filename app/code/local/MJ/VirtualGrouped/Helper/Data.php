00822c2360fa0ff0b0984f130a186b588209a5a4
Mat16eusz <59285672+Mat16eusz@users.noreply.github.com>
2021-06-05 21:07:15 +0200

    Virtual group for products in the administration panel

---
+++ app/code/local/MJ/VirtualGrouped/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Mateusz Jasiak
+ * 
+ * @author    Mateusz Jasiak
+ * @author    mateusz.jasiak.dev@gmail.com
+ *
+ * @package   MJ_Virtualgrouped
+ */
+class MJ_VirtualGrouped_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/local/MJ/VirtualGrouped/Model/Product/Type/Virtualgrouped.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Mateusz Jasiak
+ * 
+ * @author    Mateusz Jasiak
+ * @author    mateusz.jasiak.dev@gmail.com
+ *
+ * @package   MJ_Virtualgrouped
+ */
+class MJ_VirtualGrouped_Model_Product_Type_Virtualgrouped extends Mage_Catalog_Model_Product_Type_Abstract
+{
+
+}
